cc56f156d7f94a6d09b347508ddd9c363abcfba6
aryat <aryatsrivastava06@gmail.com>
2025-06-22 12:17:41 +0530

    basic setup

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/terminal-shortcut/command"
 	processinput "github.com/terminal-shortcut/process_input"
 )
 
@@ -15,15 +13,6 @@ func main() {
 		fmt.Printf("Error %s", err)
 		return
 	}
-	command.CommandRunner(&inputData)
+	// command.CommandRunner(&inputData)
 	fmt.Printf("Command: %s", inputData.Command)
-
 }
-
-//There are 3 commands to support
-// 1 rs add
-// 2 rs add foldernamd
-// 3 rs remove foldername
-// 4 rs ls
-// 5 rs help
-// 6 rs --help
--- process_input/process_input.go
+++ process_input/process_input.go
@@ -17,9 +17,20 @@ func ProcessInput(data *string) (res InputData, err error) {
 	for i, part := range parts {
 		fmt.Printf("%d, %s\n", i, part)
 	}
+	inputData := InputData{
+		Command: parts[1],
+	}
+	if (len(parts)) > constants.MaxCountOfSplitsInCommandInput {
+		inputData.Text = parts[2]
+	}
 
-	return InputData{
-		Command: "parts0",
-		Text:    "parts1",
-	}, nil
+	return inputData, nil
 }
+
+//There are 3 commands to support
+// 1 rs add
+// 2 rs add foldernamd
+// 3 rs remove foldername
+// 4 rs ls
+// 5 rs help
+// 6 rs --help
